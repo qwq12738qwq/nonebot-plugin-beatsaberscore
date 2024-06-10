95235ef6ed4995c27b91518ed94e2b4f6c11befd
qwq12738qwq <12738smp@Gmail.com>
2024-06-10 15:41:41 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
+import re
 import os
+import json
 import base64
+from collections import defaultdict
 from nonebot import on_command
 from pathlib import Path
 from playwright.async_api import async_playwright
@@ -16,9 +19,11 @@ __plugin_meta__ = PluginMetadata(
 )
 
 # 双引号里面的url自己加上去捏,登入自己的beatleader账号,在个人主页上的网址复制到引号里面就可以用了
-url = "  "
+url = "https://beatleader.xyz/u/76561198949185975"
+URL = "https://beatleader.xyz/u/"
 
 BS_Score = on_command("BS查分", aliases={"节奏光剑查分"}, priority=10)
+BS_Bind = on_command("BS绑定", aliases={"节奏光剑绑定"}, priority=10)
 
 async def BS_Score_Search(url):
 
@@ -36,16 +41,69 @@ async def BS_Score_Search(url):
 @BS_Score.handle()
 
 async def handle_BSScore(bot: Bot, event: Event):
-
+    # 读取QQ号
+    QQ_id = event.get_user_id()
+    # 读取QQ号的BS_id
+    with open('data/BSgroup.json', 'r') as json_file:
+        data = json_file.read()
+    id_search = json.loads(data)
+    id = str(id_search[QQ_id])
+    # 去除id中的中括号
+    id = id.replace("[", "").replace("]", "")
+    # 去除id中的''
+    id = id.replace("'", "").replace("'", "")
+    if not id is None:
+        url = URL + id
         # 开始截图函数
-    await BS_Score_Search(url)
+        await BS_Score_Search(url)
         # 设置存储目录(自己找自己的目录罢)
-    save_path = Path("/root/Bot/QQbot/data/") 
-    img_name = "BSScore.jpg"
-    img = save_path / img_name
+        save_path = Path("/root/Bot/QQbot/data/") 
+        img_name = "BSScore.jpg"
+        img = save_path / img_name
         # base64编码(编码才能发送图片,坑我好久QAQ)
-    with open(img, "rb") as image_file:
-         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+        with open(img, "rb") as image_file:
+             encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
-    image_base64 = f"base64://{encoded_string}"
-    await BS_Score.send(MessageSegment.image(image_base64))
+        image_base64 = f"base64://{encoded_string}"
+        await BS_Score.finish(MessageSegment.image(image_base64))
+    else:
+        msg = "该QQ账号未绑定"
+        await BS_Score.finish(msg)
+
+@BS_Bind.handle()
+
+async def handle_BS_Bind(bot: Bot, event: Event):
+    QQ_id = event.get_user_id()
+    message = str(event.get_message())
+    id_list = re.findall(r'\d+', message)
+    # 将列表的数字提取出来
+    BS_id = id_list[0]
+    # 检测是否有json文件
+    try:
+        with open('data/BSgroup.json', 'r') as f:
+            Groud_data = json.load(f)
+    except FileNotFoundError:
+        Groud_data = {}
+    # 创建字典
+    Groud_data = {
+        QQ_id: BS_id
+    }
+    # 初始化列表防止报KeyError错误
+    Groud_data = defaultdict(list)
+    Groud_data[QQ_id].append(BS_id)
+    # 写入数据到json
+    with open('data/BSgroup.json', 'w') as f:
+        json.dump(Groud_data, f, sort_keys=True, indent=4)
+    #尝试读取文件看是否写入
+    try:
+        with open('data/BSgroup.json', 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
+    if data == {}:
+        test = "数据存储失败"
+        await BS_Bind.finish(test)
+    else:
+        pass
+    msg = "QQ号:" + QQ_id + "已绑定beatleaderID:" + BS_id
+    await BS_Bind.finish(msg)
